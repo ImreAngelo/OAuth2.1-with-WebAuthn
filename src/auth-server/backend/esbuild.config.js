f45cc10ac5f76ccb2e9abec24796d77f6ee8ea2d
Imre Angelo <imre@angelo.no>
2025-09-03 19:49:20 +0200

    Made live reload more lean

--- src/auth-server/backend/esbuild.config.js
+++ src/auth-server/backend/esbuild.config.js
@@ -1,24 +1,14 @@
-const esbuild = require("esbuild");
+const { build } = require("esbuild");
 
-async function build() {
-	const ctx = await esbuild.context({
-		entryPoints: ["src/index.ts"],
-		bundle: true,
-		platform: "node",
-		sourcemap: true,
-		outfile: "dist/index.js",
-	});
-	if (process.argv.includes("--watch")) {
-		await ctx.watch();
-		console.log("👀 Watching for changes...");
-	} else {
-		await ctx.rebuild();
-		await ctx.dispose();
-		console.log("✅ Build complete");
-	}
-}
+/** Production build */
+const isOptimizeFlag = process.argv.includes("--optimize");
 
-build().catch((err) => {
-	console.error(err);
-	process.exit(1);
+build({
+	entryPoints: ["src/index.ts"],
+	bundle: true,
+	platform: "node",
+	sourcemap: !isOptimizeFlag,
+	outfile: "dist/index.js",
+}).then(() => {
+  	console.log("✅ Build succeeded");
 });
--- src/auth-server/backend/src/index.ts
+++ src/auth-server/backend/src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
-	res.send("Hello from Express + TypeScript + esbuild");
+	res.send("Hello from Express + TypeScript + esbuild!");
 });
 
 app.listen(PORT, () => {
-	console.log(`✅ Server is running at http://localhost:${PORT}`);
+	console.log(`Server is running at http://localhost:${PORT}`);
 });
